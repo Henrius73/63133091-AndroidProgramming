685f4a6bc503d7d1c6ae048b5552f0be042d8f65
Henrius73 <quanntkt2003@gmail.com>
2024-03-24 01:55:14 +0700

    GameConRan

---
+++ NguyenThienQuan_63133091/Cau1_GameConRan/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Cau1_GameConRan {
+	requires java.desktop;
+}
---
+++ NguyenThienQuan_63133091/Cau1_GameConRan/src/ntu_63133091/SnakeGame.java
@@ -0,0 +1,8 @@
+package ntu_63133091;
+
+public class SnakeGame {
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+	
+}
