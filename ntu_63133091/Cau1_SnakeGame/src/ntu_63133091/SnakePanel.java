c745db0f83b3a945aecb2556561926c841167c56
Henrius73 <quanntkt2003@gmail.com>
2024-03-24 17:32:52 +0700

    tao va gan cac nut di chuyen

--- ntu_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
+++ ntu_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
@@ -63,4 +63,41 @@ public class SnakePanel extends JPanel implements ActionListener{
             gameOver(graphic);
         }
     }
+    public class MyKey extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(dir!='R'){
+                        dir='L';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (dir != 'D') {
+                        dir = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (dir != 'L') {
+                        dir = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (dir != 'U') {
+                        dir = 'D';
+                    }
+                    break;
+                case KeyEvent.VK_R:
+                if(!game_flag){
+                    foodEAten=0;
+                    bodylength=2;
+                    dir='R';
+                   Arrays.fill(x_snake,0);
+                   Arrays.fill(y_snake,0);
+                    Game_start();
+                }
+                break;
+            }            
+        }
+    }
 }
