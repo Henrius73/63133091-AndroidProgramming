1d12a25e6327b0d1bb07d19a977a1efee3d41f26
Henrius73 <quanntkt2003@gmail.com>
2024-03-24 15:28:15 +0700

    Khai bao cac bien va ham khoi tao, chuan bi cho game

--- NguyenThienQuan_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
+++ NguyenThienQuan_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
@@ -1,5 +1,40 @@
 package ntu_63133091;
 
-public class SnakePanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import java.util.Random;
 
+public class SnakePanel extends JPanel implements ActionListener{
+	static final int S_Width=600;
+    static final int S_Height=600;
+    static final int Game_unit_size=50;
+    Timer timer;
+    Random random;
+    int foodEAten;
+    int foodX;
+    int foodY;
+    int bodylength = 2;
+    char dir = 'R';
+    static final int DELAY = 160;
+    boolean game_flag = false;
+    static final int G_Size=(S_Width*S_Height)/(Game_unit_size*Game_unit_size);
+    final int x_snake[]=new int[G_Size];
+    final int y_snake[]=new int[G_Size];
+	SnakePanel(){
+		this.setPreferredSize(new Dimension(S_Width,S_Height));
+		this.setBackground(Color.darkGray);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKey());
+		random = new Random();
+		Game_start();
+	}
+	public void Game_start() {
+		newfoodPostion();
+		game_flag=true;
+		timer = new Timer(DELAY,this);
+		timer.start();
+	}
+	
 }
