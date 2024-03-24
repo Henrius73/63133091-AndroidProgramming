f4ab068cca08663451523cab40140666577c91c0
Henrius73 <quanntkt2003@gmail.com>
2024-03-24 17:43:53 +0700

    them man hinh game over

--- ntu_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
+++ ntu_63133091/Cau1_SnakeGame/src/ntu_63133091/SnakePanel.java
@@ -31,7 +31,7 @@ public class SnakePanel extends JPanel implements ActionListener{
 		Game_start();
 	}
 	public void Game_start() {
-		newfoodPostion();
+		newfoodPosition();
 		game_flag=true;
 		timer = new Timer(DELAY,this);
 		timer.start();
@@ -90,6 +90,22 @@ public class SnakePanel extends JPanel implements ActionListener{
     	        if(!game_flag)
     	          {  timer.stop();}
     	    }
+    public void gameOver(Graphics graphic) {// When ever game is over this function will be called.
+        graphic.setColor(Color.red);
+        graphic.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics font_me = getFontMetrics(graphic.getFont());
+        graphic.drawString("Score:" + foodEAten, (S_Width - font_me.stringWidth("Score:" + foodEAten)) / 2,
+                graphic.getFont().getSize());
+        graphic.setColor(Color.red);
+        graphic.setFont(new Font("Ink Free", Font.BOLD, 75));
+        FontMetrics font_me2 = getFontMetrics(graphic.getFont());
+        graphic.drawString("Game Over", (S_Width - font_me2.stringWidth("Game Over")) / 2,
+                S_Height/2);
+                graphic.setColor(Color.red);
+        graphic.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics font_me3 = getFontMetrics(graphic.getFont());
+        graphic.drawString("Press R to Replay", (S_Width - font_me3.stringWidth("Press R to Replay")) / 2, S_Height / 2-150);
+    }
     public class MyKey extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
