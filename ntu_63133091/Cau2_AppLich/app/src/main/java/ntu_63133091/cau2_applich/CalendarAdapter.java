72abd7331c7caed49cdcd0b82aa9a55fb9bb86df
Henrius73 <quanntkt2003@gmail.com>
2024-03-25 18:02:56 +0700

    khoi tao ham de hien thi du lieu cua o lich

--- ntu_63133091/Cau2_AppLich/app/src/main/java/ntu_63133091/cau2_applich/CalendarAdapter.java
+++ ntu_63133091/Cau2_AppLich/app/src/main/java/ntu_63133091/cau2_applich/CalendarAdapter.java
@@ -1,4 +1,33 @@
 package ntu_63133091.cau2_applich;
 
-public class CalendarAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder> {
+    private final ArrayList<String> dayofMonth;
+    public CalendarAdapter(ArrayList<String> dayofMonth) {
+        this.dayofMonth = dayofMonth;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- ntu_63133091/Cau2_AppLich/app/src/main/java/ntu_63133091/cau2_applich/CalendarViewHolder.java
+++ ntu_63133091/Cau2_AppLich/app/src/main/java/ntu_63133091/cau2_applich/CalendarViewHolder.java
@@ -1,4 +1,16 @@
 package ntu_63133091.cau2_applich;
 
-public class CalendarViewHolder {
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder {
+    private final TextView dayofMonth;
+    public CalendarViewHolder(@NonNull View itemView) {
+        super(itemView);
+        dayofMonth = itemView.findViewById(R.id.cellDayText);
+    }
 }
